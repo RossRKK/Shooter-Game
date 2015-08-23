6f722534d7ccc5d814e9377abad89161586ec2cd
Ross Kelso <rosskelso@btinternet.com>
2015-08-23 16:03:32 +0100

    Set up basic canvas

---
+++ game/script.js
@@ -0,0 +1,24 @@
+//declare canvas and context objects
+var c;
+var ctx;
+$("#canvas").ready(function () {
+	c = $("#canvas").get(0);
+	ctx = c.getContext("2d");
+	//start the main game loop
+	gameLoop();
+});
+
+function render() {
+	//load next frame
+	//clear frame
+	ctx.clearRect(0,0, c.width, c.height);
+
+	ctx.fillStyle = "#010101";
+	ctx.fillRect(0, 0, c.width, c.height);
+}
+
+//the main game loop
+function gameLoop() {
+	window.requestAnimationFrame(gameLoop);
+	render();
+}
